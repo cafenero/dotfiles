e4c827ad24425d649169296076a89e9554d5fd41
cafenero <2006yusuke@gmail.com>
2015-02-03 16:41:59 +0000

    add web-mode, php-settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,3 +19,23 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+(add-hook 'php-mode-hook
+  '(lambda()
+     (setq tab-width 4)
+     (setq indent-tabs-mode t)
+     (setq c-basic-offset 4)
+   ))
+
+
+(add-to-list 'load-path "~/.emacs.d/")
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.ctp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
