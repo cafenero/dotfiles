4c8780f06459846949d842c8473c0a6a4b70d21f
cafenero <2006yusuke@gmail.com>
2015-02-06 17:04:40 +0000

    use php-mode(modoshi)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,12 +19,12 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
-;; (add-hook 'php-mode-hook
-;;   '(lambda()
-;;      (setq tab-width 4)
-;;      (setq indent-tabs-mode t)
-;;      (setq c-basic-offset 4)
-;;    ))
+(add-hook 'php-mode-hook
+  '(lambda()
+     (setq tab-width 4)
+     (setq indent-tabs-mode t)
+     (setq c-basic-offset 4)
+   ))
 
 
 (add-to-list 'load-path "~/.emacs.d/")
