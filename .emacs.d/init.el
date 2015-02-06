204c395dddc2caef07c16318b45a32c1b3a5c964
cafenero <2006yusuke@gmail.com>
2015-02-06 17:15:01 +0000

    modify php-mode setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,6 +24,10 @@
      (setq tab-width 4)
      (setq indent-tabs-mode t)
      (setq c-basic-offset 4)
+     (c-set-offset 'case-label' 4)
+     (c-set-offset 'arglist-intro' 4)
+     (c-set-offset 'arglist-cont-nonempty' 4)
+     (c-set-offset 'arglist-close' 0)
    ))
 
 
