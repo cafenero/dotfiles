f2f34eeca3662801b5e3f5230842bb65321a4e2b
cafenero <2006yusuke@gmail.com>
2015-08-25 04:25:44 +0900

    init

---
+++ .emacs.d/.emacs.d/init.el
@@ -0,0 +1,53 @@
+(setq load-path (cons "~/.emacs.d/elisp" load-path))
+
+;; default to unified diffs
+(setq diff-switches "-u")
+
+;;japanese
+;;;;; (set-language-environment "Japanese")
+;(prefer-coding-system 'utf-8)
+;(setq coding-system-for-read 'utf-8)
+;(setq coding-system-for-write 'utf-8)
+
+;;delete bar
+;(tool-bar-mode 0)
+(menu-bar-mode -1)
+
+;;save place
+(load "saveplace")
+(setq-default save-place t)
+
+;;(add-hook 'text-mode-hook 'ruler-mode)
+
+;; my keybind
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-h" 'delete-backward-char)
+
+;; make NO backup files
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(add-hook 'php-mode-hook
+  '(lambda()
+     (setq tab-width 4)
+     (setq indent-tabs-mode t)
+     (setq c-basic-offset 4)
+     (c-set-offset 'case-label' 4)
+     (c-set-offset 'arglist-intro' 4)
+     (c-set-offset 'arglist-cont-nonempty' 4)
+     (c-set-offset 'arglist-close' 0)
+   ))
+
+
+
+;; (require 'web-mode)
+;; (add-to-list 'auto-mode-alist '("\\.ctp\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(require 'go-mode)
