b9925f5d1a28a7045aed2d6dd06203cc61e29c35
cafenero <2006yusuke@gmail.com>
2018-08-29 22:08:50 +0900

    add

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,81 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.alert.show("Hello World!")
+end)
+
+
+local function keyCode(key, modifiers)
+   modifiers = modifiers or {}
+   return function()
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+      hs.timer.usleep(1000)
+      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()      
+   end
+end
+
+local function remapKey(modifiers, key, keyCode)
+   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+end
+
+local function disableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:disable()
+   end
+end
+
+local function enableAllHotkeys()
+   for k, v in pairs(hs.hotkey.getHotkeys()) do
+      v['_hk']:enable()
+   end
+end
+
+local function handleGlobalAppEvent(name, event, app)
+   if event == hs.application.watcher.activated then
+--      hs.alert.show(name)
+      if name == "iTerm2" then
+         disableAllHotkeys()
+      elseif name == "cool-retro-term" then
+         disableAllHotkeys()
+--      elseif name == "PDF Expert" then
+--         disableAllHotkeys()
+--         enableAllHotkeys()
+      else
+         enableAllHotkeys()
+      end
+   end
+end
+
+appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
+appsWatcher:start()
+
+-- Caps key to left ctrl
+-- looks not working
+remapKey({}, 'capslock', keyCode('ctrl'))
+
+
+
+-- カーソル移動
+remapKey({'ctrl'}, 'f', keyCode('right'))
+remapKey({'ctrl'}, 'b', keyCode('left'))
+remapKey({'ctrl'}, 'n', keyCode('down'))
+remapKey({'ctrl'}, 'p', keyCode('up'))
+
+-- テキスト編集
+-- remapKey({'ctrl'}, 'w', keyCode('x', {'cmd'}))
+-- remapKey({'ctrl'}, 'y', keyCode('v', {'cmd'}))
+
+-- コマンド
+remapKey({'ctrl'}, 's', keyCode('f', {'cmd'}))
+remapKey({'ctrl'}, '/', keyCode('z', {'cmd'}))
+remapKey({'ctrl'}, 'g', keyCode('escape'))
+
+-- ページスクロール
+remapKey({'ctrl'}, 'v', keyCode('pagedown'))
+remapKey({'alt'}, 'v', keyCode('pageup'))
+remapKey({'cmd', 'shift'}, ',', keyCode('home'))
+remapKey({'cmd', 'shift'}, '.', keyCode('end'))
+
+-- adding option modify
+-- remapKey({'alt'}, '2', keyCode('2'))
+
+
+
