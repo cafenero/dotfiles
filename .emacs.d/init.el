6c3a42bdead61655eb7f44e033aa7a6ae0ee5807
cafenero <2006yusuke@gmail.com>
2021-02-17 00:37:12 +0900

    mod_2021_02_17-01

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,58 @@
 ;; undo: C-/
 ;; comment: 範囲選択してからM-;
 ;; eval: C-x C-e
+;; C-x goto-line
+;; C-;をコメントアウトにしたい
+;; -> できないっぽい。。
 
 (add-to-list 'load-path	"~/.emacs.d/elisp")
+
+;; (use-package all-the-icons)
+;; (require 'all-the-icons)
+
+
+;; (load-theme 'misterioso t)
+;; (load-theme 'manoj-dark t)
+;; (load-theme 'tango-dark t)
+;; (load-theme 'wombat t)
+;; (load-theme 'wheatgrass t)
+(load-theme 'monokai t)
+;; (load-theme 'monokai-pro t)
+
+
+;; for GUI
+(setq inhibit-splash-screen t)
+
+;; 起動時のフレーム設定
+(if (boundp 'window-system)
+  (setq default-frame-alist
+    (append (list
+      '(top . 100) ;ウィンドウの表示位置(Y座標)
+      '(left . 100) ;ウィンドウの表示位置(X座標）
+      '(width . 480) ;ウィンドウ幅
+      '(height . 120) ;ウィンドウ高
+    )
+    default-frame-alist)
+  )
+)
+(setq initial-frame-alist default-frame-alist)
+
+;; alpha
+;; (if window-system
+;;     (progn
+;;       (set-frame-parameter nil 'alpha 98)))
+
+;; quickrun.elのキーバインド
+(require 'quickrun)
+(global-set-key (kbd "\C-c i") 'quickrun)
+
+;; ;; quickrun.elでPythonをPython3に
+;; (quickrun-add-command "python"
+;;   '((:command . "python3")
+;;     (:exec . "%c %s")
+;;     (:compile-only . "pyflakes %s"))
+;;   :mode 'python-mode)
+
 
 ;; 文末空白を表示
 (setq-default show-trailing-whitespace t)
@@ -14,7 +64,7 @@
 (setq diff-switches "-u")
 
 ;;delete bar
-;(tool-bar-mode 0)
+;; (tool-bar-mode 0)
 (menu-bar-mode -1)
 
 
@@ -47,6 +97,11 @@
 (global-set-key "\C-h" 'delete-backward-char)
 (global-set-key "\M-%" 'replace-string)
 
+;;(global-set-key "C-x b" 'switch-to-buffer-other-frame)
+;(global-set-key "\C-x C-b" 'switch-to-buffer-other-frame)
+;(define-key global-map (kbd "C-x C-b") 'switch-to-buffer-other-frame)
+;(define-key global-map (kbd "C-x C-b") 'switch-to-buffer-other-window)
+
 ;; make NO backup files
 (setq make-backup-files nil)
 (setq auto-save-default nil)
@@ -59,10 +114,11 @@
 ;;   (auto-install-update-emacswiki-package-name t)
 ;;   (auto-install-compatibility-setup))
 
-(when (require 'auto-install nil t)
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  (auto-install-update-emacswiki-package-name t)
-  (auto-install-compatibility-setup))
+
+;; (when (require 'auto-install nil t)
+;;   (setq auto-install-directory "~/.emacs.d/elisp/")
+;;   (auto-install-update-emacswiki-package-name t)
+;;   (auto-install-compatibility-setup))
 
 
 
@@ -231,18 +287,19 @@
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(undo-tree)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages
+;;    '(nyan-mode use-package use-package-chords use-package-el-get use-package-ensure-system-package use-package-hydra dired-sidebar all-the-icons-dired all-the-icons-gnus all-the-icons-ibuffer all-the-icons-ivy all-the-icons-ivy-rich all-the-icons neotree undo-tree)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
 
 (when (require 'undo-tree nil t)
   (global-undo-tree-mode))
@@ -262,3 +319,208 @@
     (save-buffer)
     (load-file "~/.emacs.d/init.el")
      (message "load ~/.emacs.d/init.el succeeded!" )))
+
+
+
+
+;; ;; neotree
+;; (use-package neotree
+;;   :init
+;;   (setq-default neo-keymap-style 'concise)
+;;   :config
+;;   (setq neo-smart-open t)
+;;   (setq neo-create-file-auto-open t)
+;;   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+;;   (bind-key [f8] 'neotree-toggle)
+;;   (bind-key "RET" 'neotree-enter-hide neotree-mode-map)
+;;   (bind-key "a" 'neotree-hidden-file-toggle neotree-mode-map)
+;;   (bind-key "<left>" 'neotree-select-up-node neotree-mode-map)
+;;   (bind-key "<right>" 'neotree-change-root neotree-mode-map))
+
+
+;; ;; Change neotree's font size
+;; ;; Tips from https://github.com/jaypei/emacs-neotree/issues/218
+;; (defun neotree-text-scale ()
+;;   "Text scale for neotree."
+;;   (interactive)
+;;   (text-scale-adjust 0)
+;;   (text-scale-decrease 1)
+;;   (message nil))
+;; (add-hook 'neo-after-create-hook
+;;       (lambda (_)
+;;         (call-interactively 'neotree-text-scale)))
+
+;; ;; neotree enter hide
+;; ;; Tips from https://github.com/jaypei/emacs-neotree/issues/77
+;; (defun neo-open-file-hide (full-path &optional arg)
+;;   "Open file and hiding neotree.
+;; The description of FULL-PATH & ARG is in `neotree-enter'."
+;;   (neo-global--select-mru-window arg)
+;;   (find-file full-path)
+;;   (neotree-hide))
+
+;; (defun neotree-enter-hide (&optional arg)
+;;   "Neo-open-file-hide if file, Neo-open-dir if dir.
+;; The description of ARG is in `neo-buffer--execute'."
+;;   (interactive "P")
+;;   (neo-buffer--execute arg 'neo-open-file-hide 'neo-open-dir))
+
+
+
+
+
+
+;; MELPA package installer
+;; C-x package-list-packages
+;; i x
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+
+(setq vc-follow-symlinks t)
+;(setq vc-follow-symlinks nil)
+
+
+;; no-use
+;(require 'buffer-menu-color)
+
+(define-key global-map [remap list-buffers] 'buffer-menu-other-window)
+
+
+
+
+
+;; only for GUI emacs use
+(require 'minimap)
+;; (minimap-mode 1); 常に有効にする
+(setq minimap-window-location 'right); windowの位置
+(setq minimap-update-delay 0.2); 表示を更新する時間
+(setq minimap-minimum-width 20); 幅の長さ
+;; 有効にしたいモード
+(setq minimap-major-modes '(latex-mode
+                            LaTeX-mode
+                            tex-mode
+                            TeX-mode
+                            text-mode
+                            prog-mode
+                            html-mode
+                            fundamental-mode
+                            csv-mode))
+;; (global-set-key (kbd "C-x m") 'minimap-mode); toggle
+
+
+
+
+(nyan-mode 1)
+;(nyan-mode nil)
+
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(package-selected-packages
+;;    '(monokai-alt-theme monokai-pro-theme monokai-theme popwin all-the-icons dired-toggle dired-toggle-sudo direx minimap use-package-hydra use-package-ensure-system-package use-package-el-get use-package-chords undo-tree nyan-mode neotree dired-sidebar all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
+
+
+
+;; --------
+;; Filer
+;; --------
+
+;;
+;; M-x all-the-icons
+;; M-x all-the-icons-install-fonts
+
+
+;;;; Filer.1
+;; neotree configs
+(require 'all-the-icons)
+(require 'neotree)
+;; 隠しファイルをデフォルトで表示
+(setq neo-show-hidden-files t)
+;; cotrol + q でneotreeを起動
+(global-set-key "\C-q" 'neotree-toggle)
+
+;(setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+(setq neo-theme 'icons)
+;(setq neo-theme 'arrow)
+
+
+
+
+;;;; Filer.2
+;; ; dired-siebar (simple version)
+;; (add-to-list 'load-path "path from pwd")
+;; (require 'dired-sidebar)
+;; (global-set-key "\C-q" 'dired-sidebar-toggle-sidebar)
+;; ;; (global-set-key "\C-c 1" 'dired-sidebar-toggle-sidebar)
+;; (setq dired-sidebar-subtree-line-prefix "__")
+;; (setq dired-sidebar-theme 'vscode)
+;; (setq dired-sidebar-use-term-integration t)
+;; (setq dired-sidebar-use-custom-font t)
+
+
+;; ; dired-siebar (use-package version)
+;; (use-package dired-sidebar
+;;   :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+;;   :ensure t
+;;   :commands (dired-sidebar-toggle-sidebar)
+;;   :init
+;;   (add-hook 'dired-sidebar-mode-hook
+;;             (lambda ()
+;;               (unless (file-remote-p default-directory)
+;;                 (auto-revert-mode))))
+;;   :config
+;;   (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+;;   (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+;;   (setq dired-sidebar-subtree-line-prefix "__")
+;;   (setq dired-sidebar-theme 'vscode)
+;;   (setq dired-sidebar-use-term-integration t)
+;;   (setq dired-sidebar-use-custom-font t))
+
+
+
+
+;;;; Filer.3
+;; direx
+;; diredのGUI進化版？
+;; (use-package direx)
+;; (setq direx:leaf-icon "  " direx:open-icon "- 🗂  " direx:closed-icon "+ 🗂  ")
+;; ;; (setq direx:leaf-icon "  " direx:open-icon "📂" direx:closed-icon "📁")
+;; ;; (push '(direx:direx-mode :position left :width 35 :dedicated t)
+;; ;;       popwin:special-display-config)
+;; ;; use direx-project.el
+;; ;; https://blog.shibayu36.org/entry/2013/02/12/191459
+;; ;; [f11]
+;; (bind-key
+;;  "C-x m"
+;;  (defun direx:jump-to-project-directory ()
+;;    "If in project, launch direx-project otherwise start direx."
+;;   (interactive)
+;;   (let ((result (ignore-errors
+;;                   (direx-project:jump-to-project-root-other-window)
+;;                   t)))
+;;     (unless result
+;;       (direx:jump-to-directory-other-window)))))
+
+;; (use all-the-icons)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(quickrun monokai-alt-theme monokai-pro-theme monokai-theme popwin all-the-icons dired-toggle dired-toggle-sudo direx minimap use-package-hydra use-package-ensure-system-package use-package-el-get use-package-chords undo-tree nyan-mode neotree dired-sidebar all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
