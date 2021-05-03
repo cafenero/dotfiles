632a00d63b267ffd833693d251d93eab7e980fb4
cafenero <2006yusuke@gmail.com>
2021-05-04 03:09:37 +0900

    add_mozc

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,8 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mozc-leim-title "あ")
  '(package-selected-packages
-   '(expand-region gnuplot gnuplot-mode resize-window ace-jump-mode company-lsp lsp-p4 lsp-mode lsp-ui restart-emacs persp-mode save-visited-files uuidgen markdown-preview-mode point-stack flycheck leaf elscreen zoom-window anzu magit pcap-mode undo-tree rotate php-mode dockerfile-mode yaml-mode go-mode all-the-icons web-mode jenkinsfile-mode dirtree yasnippet use-package-hydra use-package-ensure-system-package use-package-el-get use-package-chords quickrun popwin nyan-mode neotree monokai-theme monokai-pro-theme monokai-alt-theme minimap direx dired-toggle-sudo dired-toggle dired-sidebar all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired))
+   '(lua-mode mozc-popup mozc-im mozc expand-region gnuplot gnuplot-mode resize-window ace-jump-mode company-lsp lsp-p4 lsp-mode lsp-ui restart-emacs persp-mode save-visited-files uuidgen markdown-preview-mode point-stack flycheck leaf elscreen zoom-window anzu magit pcap-mode undo-tree rotate php-mode dockerfile-mode yaml-mode go-mode all-the-icons web-mode jenkinsfile-mode dirtree yasnippet use-package-hydra use-package-ensure-system-package use-package-el-get use-package-chords quickrun popwin nyan-mode neotree monokai-theme monokai-pro-theme monokai-alt-theme minimap direx dired-toggle-sudo dired-toggle dired-sidebar all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired))
  '(safe-local-variable-values
    '((eval setq flycheck-clang-language-standard "c++17")
 	 (eval setq flycheck-clang-include-path include-directories)
