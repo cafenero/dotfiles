31a1c12d4beb1f5c10681e19ce1aa4d7de10d260
cafenero <2006yusuke@gmail.com>
2021-05-29 12:16:32 +0900

    emacs-test

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,6 +36,31 @@
 
 
 ;; experimantal
+(selectrum-mode +1)
+
+
+;; experimantal
+;; (use-package icomplete-vertical
+;;   :ensure t
+;;   :demand t
+;;   :custom
+;;   (completion-styles '(partial-completion substring))
+;;   (completion-category-overrides '((file (styles basic substring))))
+;;   (read-file-name-completion-ignore-case t)
+;;   (read-buffer-completion-ignore-case t)
+;;   (completion-ignore-case t)
+;;   :config
+;;   (icomplete-mode)
+;;   (icomplete-vertical-mode)
+;;   :bind (:map icomplete-minibuffer-map
+;;               ("<down>" . icomplete-forward-completions)
+;;               ("C-n" . icomplete-forward-completions)
+;;               ("<up>" . icomplete-backward-completions)
+;;               ("C-p" . icomplete-backward-completions)
+;;               ("C-v" . icomplete-vertical-toggle)))
+
+
+;; experimantal
 (defun insert-current-date (&optional diff)
   "現在年月日をカレントバッファに出力します。引数Nを与えるとN日前を出力します。"
   (interactive "P")
