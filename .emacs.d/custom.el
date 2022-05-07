3d1f06b271ea8ef05b95fe76a32dc167c95188de
cafenero <2006yusuke@gmail.com>
2022-05-07 21:27:47 +0900

    fix init.el file sepalation

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,23 +5,23 @@
  ;; If there is more than one, they won't work right.
  '(mozc-leim-title "も")
  '(package-selected-packages
-   '(graphviz-dot-mode company ccls python-mode selectrum icomplete-vertical lua-mode mozc-popup mozc-im mozc expand-region gnuplot gnuplot-mode resize-window ace-jump-mode company-lsp lsp-p4 lsp-mode lsp-ui restart-emacs persp-mode save-visited-files uuidgen markdown-preview-mode point-stack flycheck leaf elscreen zoom-window anzu magit pcap-mode undo-tree rotate php-mode dockerfile-mode yaml-mode go-mode all-the-icons web-mode jenkinsfile-mode dirtree yasnippet use-package-hydra use-package-ensure-system-package use-package-el-get use-package-chords quickrun popwin nyan-mode neotree monokai-theme monokai-pro-theme monokai-alt-theme minimap direx dired-toggle-sudo dired-toggle dired-sidebar all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired))
+   '(xcscope graphviz-dot-mode company ccls python-mode selectrum icomplete-vertical lua-mode mozc-popup mozc-im mozc expand-region gnuplot gnuplot-mode resize-window ace-jump-mode company-lsp lsp-p4 lsp-mode lsp-ui restart-emacs persp-mode save-visited-files uuidgen markdown-preview-mode point-stack flycheck leaf elscreen zoom-window anzu magit pcap-mode undo-tree rotate php-mode dockerfile-mode yaml-mode go-mode all-the-icons web-mode jenkinsfile-mode dirtree yasnippet use-package-hydra use-package-ensure-system-package use-package-el-get use-package-chords quickrun popwin nyan-mode neotree monokai-theme monokai-pro-theme monokai-alt-theme minimap direx dired-toggle-sudo dired-toggle dired-sidebar all-the-icons-ivy-rich all-the-icons-ivy all-the-icons-ibuffer all-the-icons-gnus all-the-icons-dired))
  '(py-indent-offset 4)
  '(safe-local-variable-values
    '((eval progn
-		   (pp-buffer)
-		   (indent-buffer))
-	 (eval setq flycheck-clang-language-standard "c++17")
-	 (eval setq flycheck-clang-include-path include-directories)
-	 (eval set
-		   (make-local-variable 'include-directories)
-		   (list
-			(expand-file-name
-			 (concat project-directory "src"))))
-	 (eval set
-		   (make-local-variable 'project-directory)
-		   (file-name-directory
-			(locate-dominating-file default-directory ".dir-locals.el"))))))
+           (pp-buffer)
+           (indent-buffer))
+     (eval setq flycheck-clang-language-standard "c++17")
+     (eval setq flycheck-clang-include-path include-directories)
+     (eval set
+           (make-local-variable 'include-directories)
+           (list
+            (expand-file-name
+             (concat project-directory "src"))))
+     (eval set
+           (make-local-variable 'project-directory)
+           (file-name-directory
+            (locate-dominating-file default-directory ".dir-locals.el"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
