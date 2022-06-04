f8285a32e63f837f23ed6378ff2eb7c0668dbb8d
cafenero <2006yusuke@gmail.com>
2022-06-04 13:06:14 +0900

    use docker latest

--- .init.sh
+++ .init.sh
@@ -7,40 +7,56 @@ case ${OS} in
         sudo apt -y install \
              emacs-nox tree vim tig ctags htop \
              linux-doc tmux emacs-mozc \
-             termshark fzf docker-compose jq
+             termshark fzf jq apt-utils
+        # sudo apt -y install docker-compose
         wget https://github.com/gsamokovarov/jump/releases/download/v0.40.0/jump_0.40.0_amd64.deb && sudo dpkg -i jump_0.40.0_amd64.deb && rm jump_0.40.0_amd64.deb
 
-        # install latest git
+        # docker latest
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        echo \
+            "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+              $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt update
+        sudo apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+        sudo systemctl restart docker
+
+        # latest git
         sudo add-apt-repository -y ppa:git-core/ppa
         sudo apt update
         sudo apt -y install git
 
-
-        # golang
+        # latest golang
         sudo add-apt-repository -y ppa:longsleep/golang-backports
         sudo apt update
         sudo apt -y install golang-go
 
-        # tools: gh, ghq, tmux
+        # gh, ghq, tmux
         # see https://github.com/cli/cli/blob/trunk/docs/install_linux.md
         curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
         sudo apt update
         sudo apt install gh
         go install github.com/x-motemen/ghq@latest
-		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
         # dev packages
         sudo apt -y install autoconf autogen autopoint libglib2.0-dev libtool xsltproc libsemanage-dev make bison gettext
 
-        # emacs init
+        # init emacs
         emacs -e 'my-allinstall' -e 'kill-emacs'
         ;;
     *CentOS*)
         sudo yum install --enablerepo=epel -y \
              git emacs-nox tree vim tig ctags htop \
              kernel-doc mozc
-		git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+        # IUSを使うパターン
+        # sudo yum -y remove git
+        # sudo yum install libsecret
+        # sudo yum -y install git  --disablerepo=\* --enablerepo=ius
+
+        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
         if [ -e ~/tmux.tar.gz ]; then
             tar xf tmux.tar.gz
             cd tmux
