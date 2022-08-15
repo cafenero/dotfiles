2e16670c031eb063d85cc180fad6937cbb89b538
cafenero <2006yusuke@gmail.com>
2022-08-16 02:50:06 +0900

    mod numjobs and re-benchmark

--- script/fio-benchmark/benchmark-fio.sh
+++ script/fio-benchmark/benchmark-fio.sh
@@ -8,15 +8,15 @@ mkdir -p $DIR
 date > $DIR/date.txt
 
 # bs=4KB, read/randread
-fio -filename=/tmp/test10G -direct=1 -rw=read      -bs=4k -size=10G -numjobs=64 -runtime=10 -group_reporting -name=seqR-4KB  --output-format=json > $DIR/seqR-4KB.txt
+fio -filename=/tmp/test10G -direct=1 -rw=read      -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqR-4KB  --output-format=json > $DIR/seqR-4KB.txt
 sleep 1
-fio -filename=/tmp/test10G -direct=1 -rw=randread  -bs=4k -size=10G -numjobs=64 -runtime=10 -group_reporting -name=randR-4KB  --output-format=json > $DIR/randR-4KB.txt
+fio -filename=/tmp/test10G -direct=1 -rw=randread  -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randR-4KB  --output-format=json > $DIR/randR-4KB.txt
 sleep 1
 
 # bs=4KB, write/randwrite
-fio -filename=/tmp/test10G -direct=1 -rw=write     -bs=4k -size=10G -numjobs=64 -runtime=10 -group_reporting -name=seqW-4KB  --output-format=json > $DIR/seqW-4KB.txt
+fio -filename=/tmp/test10G -direct=1 -rw=write     -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqW-4KB  --output-format=json > $DIR/seqW-4KB.txt
 sleep 1
-fio -filename=/tmp/test10G -direct=1 -rw=randwrite -bs=4k -size=10G -numjobs=64 -runtime=10 -group_reporting -name=randW-4KB  --output-format=json > $DIR/randW-4KB.txt
+fio -filename=/tmp/test10G -direct=1 -rw=randwrite -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randW-4KB  --output-format=json > $DIR/randW-4KB.txt
 sleep 1
 
 
