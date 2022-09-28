0cbc10d525f6565302375edc4153b16eeebb0fd8
cafenero <2006yusuke@gmail.com>
2022-09-28 22:28:02 +0900

    add NVMe result

--- script/fio-benchmark/benchmark-fio.sh
+++ script/fio-benchmark/benchmark-fio.sh
@@ -7,27 +7,30 @@ mkdir -p $DIR
 
 date > $DIR/date.txt
 
+tmpDIR=$2
+
+
 # bs=4KB, read/randread
-fio -filename=/tmp/test10G -direct=1 -rw=read      -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqR-4KB  --output-format=json > $DIR/seqR-4KB.txt
+fio -filename=$2/test10G -direct=1 -rw=read      -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqR-4KB  --output-format=json > $DIR/seqR-4KB.txt
 sleep 1
-fio -filename=/tmp/test10G -direct=1 -rw=randread  -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randR-4KB  --output-format=json > $DIR/randR-4KB.txt
+fio -filename=$2/test10G -direct=1 -rw=randread  -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randR-4KB  --output-format=json > $DIR/randR-4KB.txt
 sleep 1
 
 # bs=4KB, write/randwrite
-fio -filename=/tmp/test10G -direct=1 -rw=write     -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqW-4KB  --output-format=json > $DIR/seqW-4KB.txt
+fio -filename=$2/test10G -direct=1 -rw=write     -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqW-4KB  --output-format=json > $DIR/seqW-4KB.txt
 sleep 1
-fio -filename=/tmp/test10G -direct=1 -rw=randwrite -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randW-4KB  --output-format=json > $DIR/randW-4KB.txt
+fio -filename=$2/test10G -direct=1 -rw=randwrite -bs=4k -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randW-4KB  --output-format=json > $DIR/randW-4KB.txt
 sleep 1
 
 
 # bs=32MB, read/randread
-fio -filename=/tmp/test10G -direct=1 -rw=read      -bs=32m -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqR-32MB  --output-format=json > $DIR/seqR-32MB.txt
+fio -filename=$2/test10G -direct=1 -rw=read      -bs=32m -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqR-32MB  --output-format=json > $DIR/seqR-32MB.txt
 sleep 1
-fio -filename=/tmp/test10G -direct=1 -rw=randread  -bs=32m -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randR-32MB  --output-format=json > $DIR/randR-32MB.txt
+fio -filename=$2/test10G -direct=1 -rw=randread  -bs=32m -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randR-32MB  --output-format=json > $DIR/randR-32MB.txt
 sleep 1
 
 # bs=32MB, write/randwrite
-fio -filename=/tmp/test10G -direct=1 -rw=write     -bs=32m -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqW-32MB  --output-format=json > $DIR/seqW-32MB.txt
+fio -filename=$2/test10G -direct=1 -rw=write     -bs=32m -size=10G -numjobs=16 -runtime=10 -group_reporting -name=seqW-32MB  --output-format=json > $DIR/seqW-32MB.txt
 sleep 1
-fio -filename=/tmp/test10G -direct=1 -rw=randwrite -bs=32m -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randW-32MB  --output-format=json > $DIR/randW-32MB.txt
+fio -filename=$2/test10G -direct=1 -rw=randwrite -bs=32m -size=10G -numjobs=16 -runtime=10 -group_reporting -name=randW-32MB  --output-format=json > $DIR/randW-32MB.txt
 sleep 1
